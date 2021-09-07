dcd0e8357739f3b757bbffb6c2afe2f830384a81
Luke Snider <originaldotbuilders@gmail.com>
2021-09-07 16:22:14 -0700

    stuff

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,6 +7,15 @@
         </h1>
       </div>
     </div>
+    <div class="footer container h-4 m-auto">
+      <div class="flex w-full justify-end">
+          <div class="flex w-1/4 justify-center">
+            <a class="h-full" href="#" aria-label="Go To My Twitter Page">
+              <img class="w-6 h-full" src="twitter-white.png"  alt="Twitter Logo"/>
+            </a>
+          </div>
+      </div>
+    </div>
   </div>
 </template>
 
