f8ec404783fb88a8de9e4da16b0ca835e238e8f1
Luke Snider <luke946@gmail.com>
2022-03-12 10:10:52 -0800

    removed line animations and made footer fixed for now

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="bg-black text-white font-raleway h-screen md:min-h-screen flex flex-col">  
-    <main id="main" class="w-full flex-grow">
+  <div class="bg-black text-white font-raleway flex flex-col">  
+    <main id="main" class="w-full flex-grow min-h-screen">
       <div class="mr-10 md:mr-24">
         <div class="flex justify-center mt-10">
           <img class="w-16 md:w-36 rounded-full" src="lukesnider.jpeg" alt="Luke Snider " />
@@ -22,14 +22,17 @@
         </div>
       </div>
     </main>
-    <footer class="flex h-32">
+    <footer class="flex h-32 fixed bottom-0 right-0 left-0">
       <div class="absolute social-top right-10 md:right-24"></div>
-      <div class="h-10 flex w-full justify-end p-6 h-full">
+      <div class="flex w-full justify-end p-6 h-full">
         <div class="z-50 w-full flex items-center justify-end mr-16 md:mr-32 lg:mr-64">
-          <a target="_blank" class="mx-2 z-50 social-icon-wrapper flex justify-center items-center " href="https://twitter.com/lsunkieder" aria-label="Go To My Twitter Page">
+          <a target="_blank" class="mx-2 z-50 social-icon-wrapper flex justify-center items-center " href="https://github.com/lukesnider" aria-label="Go To My Github Profile">
+            <img class="social-icon" src="github-white.png"  alt="Github Logo"/>
+          </a>
+          <a target="_blank" class="mx-2 z-50 social-icon-wrapper flex justify-center items-center " href="https://twitter.com/lsunkieder" aria-label="Go To My Twitter Profile">
             <img class="social-icon" src="twitter-white.png"  alt="Twitter Logo"/>
           </a>
-          <a target="_blank" class="mx-2 z-50 social-icon-wrapper flex justify-center items-center " href="https://www.linkedin.com/in/luke-snider-08262871" aria-label="Go To My Linkedin Page">
+          <a target="_blank" class="mx-2 z-50 social-icon-wrapper flex justify-center items-center " href="https://www.linkedin.com/in/luke-snider-08262871" aria-label="Go To My Linkedin Profile">
             <img class="social-icon" src="linkedin-white.png"  alt="Linkedin Logo"/>
           </a>
         </div>
@@ -95,13 +98,13 @@ export default {
   animation: slideRight 3s ease 0s 1 normal forwards;
 }
 .social-left {
-  animation: slideright 2.5s ease 0s 1 normal forwards;
+  /* animation: slideright 2.5s ease 0s 1 normal forwards; */
   border-top: solid 3px white;
   left:0;
   bottom: 16%;
 }
 .social-top {
-  animation: slidedown 3s ease 0s 1 normal forwards;
+  /* animation: slidedown 3s ease 0s 1 normal forwards; */
   border-right: solid 3px white;
   top: 0;
 }
